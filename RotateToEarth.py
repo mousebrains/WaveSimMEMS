6b95d8ec929600cc8de7615fcd7cfe5db6287d08
Pat Welch <pat@mousebrains.com>
2021-03-24 14:45:25 -0700

    Notational changes

--- RotateToEarth.py
+++ RotateToEarth.py
@@ -15,7 +15,7 @@ def rotate(a:pd.DataFrame) -> pd.DataFrame:
     #  y -> Northward
     #  z -> Vertical (This is the same in earth and wave coordinates
 
-    hdg = np.radians(a["hdg"]) # Heading in radians
+    hdg = np.radians(a.hdg) # Heading in radians
     shdg = np.sin(hdg)
     chdg = np.cos(hdg)
 
@@ -23,31 +23,31 @@ def rotate(a:pd.DataFrame) -> pd.DataFrame:
     df["t"] = a["t"]
     df["hdg"] = a["hdg"]
 
-    # Rotate position from wave to earth
-    df["x"] = a["wx"] * shdg # Eastward
-    df["y"] = a["wx"] * chdg # Northward
-    df["z"] = a["wz"]        # Vertical
+    # Rotate position from wave to earth, +y -> North, +x -> East
+    df["x"] = a.wx * shdg # Eastward
+    df["y"] = a.wx * chdg # Northward
+    df["z"] = a.wz        # Vertical
 
     # Rotate velocity from wave to earth
-    df["vx"] = a["wvx"] * shdg # Eastward
-    df["vy"] = a["wvx"] * chdg # Northward
-    df["vz"] = a["wvz"] * chdg # Vertical
+    df["vx"] = a.wvx * shdg # Eastward
+    df["vy"] = a.wvx * chdg # Northward
+    df["vz"] = a.wvz        # Vertical
 
     # Rotate acceleration from wave to earth
-    df["ax"] = a["wax"] * shdg # Eastward
-    df["ay"] = a["wax"] * chdg # Northward
-    df["az"] = a["waz"]        # Vertical
+    df["ax"] = a.wax * shdg # Eastward
+    df["ay"] = a.wax * chdg # Northward
+    df["az"] = a.waz        # Vertical
 
     # Note, angular velocity and acceleration are in wy direction,
     # Rotate angular velocity from wave to earth
-    df["omegax"] =  a["wOmega"] * chdg # Eastward
-    df["omegay"] = -a["wOmega"] * shdg # Northward
-    df["omegaz"] = np.zeros(a["wOmega"].shape) # Vertical always zero
+    df["omegax"] =  a.wOmega * chdg # Eastward
+    df["omegay"] = -a.wOmega * shdg # Northward
+    df["omegaz"] = np.zeros(a.wOmega.shape) # Vertical always zero
 
     # Rotate angular acceleration from wave to earth
-    df["alphax"] =  a["wAlpha"] * chdg # Eastward
-    df["alphay"] = -a["wAlpha"] * shdg # Northward
-    df["alphaz"] = np.zeros(a["wAlpha"].shape) # Vertical always zero
+    df["alphax"] =  a.wAlpha * chdg # Eastward
+    df["alphay"] = -a.wAlpha * shdg # Northward
+    df["alphaz"] = np.zeros(a.wAlpha.shape) # Vertical always zero
 
     return df
 
@@ -56,14 +56,23 @@ def saveCSV(fn:str, name:str, df:pd.DataFrame) -> None:
     ofn = "{}.{}.earth.csv".format(prefix, name)
     df.to_csv(ofn, index=False)
 
+def plotit(ax, df, x, y):
+    ax.plot(df[x], df[y], "-")
+    ax.grid()
+    ax.set_xlabel(x)
+    ax.set_ylabel(y)
+    ax.set_aspect(1)
+
 if __name__ == "__main__":
     import argparse
     import MyLogger
+    import matplotlib.pyplot as plt
 
     parser = argparse.ArgumentParser()
     parser.add_argument("yaml", metavar="fn.yml", help="YAML configuratioin file")
     parser.add_argument("wave", metavar="fn.csv", help="Output of mkEllipse to load")
     parser.add_argument("--save", action="store_true", help="Should CSV output files be generated?")
+    parser.add_argument("--plot", action="store_true", help="Some diagnostic plots")
     MyLogger.addArgs(parser)
     args = parser.parse_args()
 
@@ -77,5 +86,16 @@ if __name__ == "__main__":
 
     df = rotate(a)
     logger.info("Earth %s\n%s", name, df)
+
     if args.save:
         saveCSV(args.yaml, name, df)
+
+    if args.plot:
+        (fig, ax) = plt.subplots(3,2, figsize=(10,10))
+        plotit(ax[0,0], df, "x", "z")
+        plotit(ax[1,0], df, "y", "z")
+        plotit(ax[2,0], df, "x", "y")
+        plotit(ax[0,1], df, "vx", "vz")
+        plotit(ax[1,1], df, "vy", "vz")
+        plotit(ax[2,1], df, "vx", "vy")
+        plt.show()
