e28b8600ada21cca016811ab257a4a1d4beb2c69
Pat Welch <pat@mousebrains.com>
2021-03-24 14:45:55 -0700

    Add polar directional spectra plots

--- simWaves.py
+++ simWaves.py
@@ -7,9 +7,13 @@
 import argparse
 import MyLogger
 import MyProcess
+import MyPolar
 
 parser = argparse.ArgumentParser(description="Simulate a series of surface waves")
 parser.add_argument("yaml", nargs="+", metavar="fn.yml", help="YAML file(s) to simulate")
+parser.add_argument("--polar", action="store_true", help="Plot directional spectrum of results")
+parser.add_argument("--threshold", type=float, default=0.01,
+        help="Polar plot colorbar threshold")
 MyProcess.addArgs(parser)
 MyLogger.addArgs(parser)
 args = parser.parse_args()
@@ -18,5 +22,8 @@ logger = MyLogger.mkLogger(args, __name__, "%(asctime)s: %(levelname)s - %(messa
 
 for fn in args.yaml:
     logger.info("Processing %s", fn)
-    MyProcess.process(fn, args, logger)
+    info = MyProcess.process(fn, args, logger)
     logger.info("Done processing %s", fn)
+
+    if args.polar and (info is not None):
+        MyPolar.plotit(info, threshold=args.threshold)
